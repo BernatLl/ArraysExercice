c06a19757d883cbb394394834c5de523e38dcfff
BernatLl <blloret@hotmail.com>
2022-01-18 13:46:20 +0000

    update

--- src/app.js
+++ src/app.js
@@ -1,11 +1,66 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const dataset1 = [
+  1,
+  1,
+  10,
+  34,
+  21,
+  10,
+  38,
+  1,
+  10,
+  21,
+  null,
+  undefined,
+  function() {}
+];
+const dataset2 = ["mama", "papa", "perro", "gato"];
+//1
+const onlynumbers = dataset1.filter(arr => typeof arr == "number");
+console.log(onlynumbers);
+//2
+const EliminaDuplicados = arr => {
+  return [...new Set(arr)];
 };
+console.log(EliminaDuplicados(dataset1));
+
+//3
+function contarLetras(letras) {
+  var objeto = {};
+  for (var i in letras) {
+    for (var j in letras[i]) {
+      objeto[letras[i][j]] = (objeto[letras[i][j]] || 0) + 1;
+    }
+  }
+  return objeto;
+}
+console.log(contarLetras(dataset2));
+
+//4
+const reorder = dataset2.reverse();
+console.log(dataset2);
+
+//5
+const reverseString = str =>
+  str
+    .split("")
+    .reverse()
+    .join("");
+
+const ReOrder = dataset2.reverse();
+const reverseArray = array => array.map(a => reverseString(a));
+
+console.log(reverseArray(dataset2));
+
+/*
+1 - Crees una funcion que limpie el array (elmine todo lo que no sean numeros) 
+2 - Crees una funcion que devuelva el array sin repeticiones dataset1
+3 - cuente las repeticiones de letras en el dataset2
+4 - que me devuleva el array al reves
+5 - que me devuelva el array al reves pero con las palabras al reves
+
+// REGLAS
+* Usar programacion funcional (no usar objetos)
+* Aplicar currificacion
+* Prohibido break en los bucles
+
+*/
